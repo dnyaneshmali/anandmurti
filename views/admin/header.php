6b53d431ea109d1d3e7cd0ab9564d22bd36fa8f1
dnyaneshmali <dnyanmali@gmail.com>
2017-02-02 12:06:11 +0530

    added ngstorage local file

--- views/admin/header.php
+++ views/admin/header.php
@@ -24,9 +24,9 @@ include($root."/config/config.php");
     <script src="<?php echo base_url(); ?>/assets/js/angular-route.min.js"></script>
     <script src="<?php echo base_url(); ?>/config/app.js"></script>
     <script src="<?php echo base_url(); ?>/controllers/globalcontrollers.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ngStorage/0.3.6/ngStorage.min.js"></script>  
+    <script src="<?php echo base_url(); ?>/assets/js/ngStorage.min.js"></script>  
     <script src="<?php echo base_url(); ?>/controllers/aqua/aquacontrollers.js"></script>  
-      <script src="<?php echo base_url(); ?>/controllers/gogas/gogascontrollers.js"></script>  
+    <script src="<?php echo base_url(); ?>/controllers/gogas/gogascontrollers.js"></script>  
   </head>
   <body class="nav-md" ng-app="app">
     <div class="container body">
