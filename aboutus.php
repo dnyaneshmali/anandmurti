36dfd32804197524c00c1916397cb3a30b4cb1e6
monica <monicamca3@gmail.com>
2017-02-15 11:47:20 +0530

    icon size

--- aboutus.php
+++ aboutus.php
@@ -10,14 +10,15 @@
    <div class="container ">
       <div class="row" >
          <div class="col-md-8 col-sm-12">
-            <div Class="customDiv"> <img src="images/img.jpg" width="128" height="128" class="owner_img"></br>
+            <div Class="customDiv latofont"> <img src="images/img.jpg" width="128" height="128" class="owner_img"></br>
                Welcome to Anandmuti,  We’re dedicated to giving you the very best of  quality water ,lpg gas and food beverages, with a focus on dependability, customer service and uniqueness.
                Founded in [year] by Mr.Pramod Baradkar. When Mr.Pramod Baradkar,first started out, his passion for making rural life healthy and easy , providing the best quality water and lpg gas for domestic and industrial use drove him to action, and gave him the impetus to turn hard work and inspiration. We now serve customers all over Natepute, and are thrilled to be a part of the rural development.  When Mr.Pramod Baradkar,first started out, his passion for making rural life healthy and easy , providing the best quality water and lpg gas for domestic and industrial use drove him to action, and gave him the impetus to turn hard work and inspiration. We now serve customers all over Natepute, and are thrilled to be a part of the rural development. 
             </div>
          </div>
          <div class="col-md-4 col-sm-12">
             <div class="customDiv">
-               <h3>Our Mission</h3>
+               <h3><span class="fa fa-bullseye" aria-hidden="true">
+                       </span>Our Mission</h3>
                <p>The AnandMurti was started with the aim to produce big market in rural area and manage the all busiess.</p>
             </div>
          </div>
