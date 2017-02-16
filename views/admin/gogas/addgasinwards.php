4209afa058b203a501ec98d5df89914f9b1ae817
dnyaneshmali <dnyanmali@gmail.com>
2017-02-16 17:41:53 +0530

    udpdated

--- views/admin/gogas/addgasinwards.php
+++ views/admin/gogas/addgasinwards.php
@@ -32,12 +32,23 @@
                          <div class=" form-group col-md-6 col-sm-6 col-xs-12" >
                           <div class="input-group">
                              <span class="input-group-addon"><i class="fa fa-rupee"></i></span>
-                          <input type="text" placeholder=" Total Price" ng-model="gasinwards.tprice" id="tprice" class="form-control" name="tprice" required value="productprice.product_price" />
+                          <input  type="text" placeholder=" Total Price" ng-model="gasinwards.tprice" id="tprice" class="form-control" name="tprice" required/>
                         </div>
                           <p class="val-style" ng-show="inwardsform.tprice.$invalid && !inwardsform.tprice.$pristine" class="help-block"> required</p>
                         </div>
                       </div>
-                    
+                      <div class="item form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="tprice">Total Price<span class="required">*</span>
+                        </label>
+                        <div class=" form-group col-md-6 col-sm-6 col-xs-12" >
+                          <div class="input-group">
+                          <span class="input-group-addon"><i class="fa fa-rupee"></i></span>
+                      <div ng-repeat="price in productprice">
+                      <input type="text" name="t1" value="{{price.product_price}}">
+                      </div>
+                      </div>
+                      </div>
+                      </div>
                        <div class="item form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="distributor_name">Distributor Name<span class="required">*</span>
                         </label>
@@ -61,7 +72,6 @@
                         </div>
                         </div>
                       </div>
-
                       <div class="ln_solid"></div>
                       <div class="form-group">
                         <div class="col-md-6 col-md-offset-5 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-4">
