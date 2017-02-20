631e994568544118f4d4c92f04943e3808d29756
monica <monicamca3@gmail.com>
2017-02-20 17:35:00 +0530

    designing

---
+++ dummy-contact.php
@@ -0,0 +1,117 @@
+<?php include('header.php');?>
+<!--End of main div-->
+<style>.m-b-100{margin-bottom: 100px;
+   margin-top: 100px}
+/*   .container.bgclr {
+    background-color: #b6d6a6;
+}*/.col-lg-12.bgclr {
+/*    background-color: #b6d6a6;*/
+    background-color: #f1f3f0;
+    /*padding-top: 70px;*/
+
+    margin-top: 20px;
+}</style>
+<div class="page-content contact-page">
+  
+  <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 bgclr">
+                   <!--  <hr>
+                    <h2 class="intro-text text-center">Contact
+                        <strong>business casual</strong>
+                    </h2>
+                    <hr> --> <h2>
+                                 <center>Contact us</center>
+                                  <div class="underline" style="width:25%; margin-bottom: 80px;"></div>
+                              </h2>
+                
+                <div class="col-md-8">
+                    <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
+                    <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.citymaps.ie/create-google-map/map.php?width=100%&amp;height=600&amp;hl=en&amp;q=natepute%20solapur%20maharashtra+(%20Anandmurti%20Group)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=A&amp;output=embed" style="border:1px solid gray;">
+                              </iframe>
+                </div>
+                <div class="col-md-4">
+                    <p><i class="fa fa-phone"></i>  Phone:
+                        <strong>123.456.7890</strong>
+                    </p>
+                    <p><i class="fa fa-envelope"></i>  Email:
+                        <strong><a href="mailto:name@example.com">name@example.com</a></strong>
+                    </p>
+                    <p><i class="fa fa-home"></i>  Address 1:
+                        <strong>3481 Melrose Place
+                            <br>Beverly Hills, CA 90210 Beverly Hills, 
+                            <br>Beverly Hills, CA 90210 Beverly Hills</strong>
+                    </p><br>
+                     <p><i class="fa fa-home"></i>  Address 1:
+                        <strong>3481 Melrose Place
+                            <br>Beverly Hills, CA 90210
+                            <br>Beverly Hills, CA 90210</strong>
+                    </p>
+                </div>
+                <div class="clearfix m-b-100"></div>
+            </div>
+        </div>
+     </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 bgclr">
+                  
+                    <h2 class="intro-text text-center">Contact
+                        <strong>form</strong>
+                        <div class="underline" style="width:25%;"></div>
+                    </h2>
+                   
+                    <p style= "margin-bottom: 80px;">Thanks for interest in AnnandMurti group of companies.Please use this form if you have any questions about our products and we'll get back with you very soon.questions about our products and we'll get back with you very soon.</p>
+                    <form role="form">
+                        <div class="row">
+                            <div class="form-group col-lg-4">
+                                <label>Name</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group col-lg-12">
+                                <label>Message</label>
+                                <textarea class="form-control" rows="6"></textarea>
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <input type="hidden" name="save" value="contact">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+               </div>
+            </div>
+         </div>
+<!--       </div>
+   </div>
+</div>
+</div> -->
+<!--  <div class="footer-cta-section">
+   <div class="container">
+   <div class="row">
+   <div class="col-md-9">
+   <h3>Click Here To Contact And To Know More !</h3>
+   </div>
+   <div class="col-md-3">
+   <a class="footer-cta-btn btn btn-lg" href="#">CHECK IT</a>
+   </div>
+   </div>
+   </div>
+   </div> -->
+<?php include('footer.php');?>
