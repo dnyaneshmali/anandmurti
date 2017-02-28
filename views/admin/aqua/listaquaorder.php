faa59bfbe358a53165e2d0c53fb12c79a9728434
dnyaneshmali <dnyanmali@gmail.com>
2017-02-28 12:04:02 +0530

    changes invoice

--- views/admin/aqua/listaquaorder.php
+++ views/admin/aqua/listaquaorder.php
@@ -101,15 +101,19 @@
     padding-top: 10px;
     padding-bottom: 25px;
 }
-.id-section {
-    text-align: center;
-}
+
 .invoice-title{
     background-color: #66B845;
     margin-bottom: 24px;
     color: #fff;
     padding: 10px;
   }
+  .in-footer-right {
+    float: right;
+}
+h3.invoice-title {
+    text-align: center;
+}
 
 
   @media screen {
@@ -153,7 +157,7 @@
                         <h5>Fax: (120) 4609464, 4609496</h5>
                         </div>
                         <div class="row">
-                        <h5><strong>Invoice ID:</strong> IN-001</h5>
+                        <h5><strong>Invoice ID:</strong> {{cinvoiceid}}</h5>
                         <h5><strong>Invoice Date:</strong> {{cdate | date:'yyyy-MM-dd'}}</h5>
                         <h5><strong>Due Date:</strong> <input type="date" placeholder="Delivery Date" class="form-control" ng-model="duedate" id="duedate" style="width: 50%;" class="form-control" name="cdate" required /></h5>
                         </div>
