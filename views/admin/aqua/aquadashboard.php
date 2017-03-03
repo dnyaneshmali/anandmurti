5f4961eed5ebe29d8b659406965631c8dd00fa20
prashantmaskar <prashant.maskar@softinfology.com>
2017-03-03 12:12:11 +0530

    grpah changes

--- views/admin/aqua/aquadashboard.php
+++ views/admin/aqua/aquadashboard.php
@@ -1,9 +1,86 @@
 <div ng-controller="Aquadashboardctrl">
 <div class ="row top_tiles">
-  <div class="col-lg-12 col-md-12 col-xs-12">
-  <div google-chart chart="chartObject" style="height:400px;
-width:100%;"></div>
 
+<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+          <div class="x_panel">
+                  <div class="x_title">
+                    <h2> Aqua Orders <small>Monthly</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+         <div google-chart chart="chartObject" style="
+width:100%;"></div>
+                  </div>
+                </div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+          <div class="x_panel">
+                  <div class="x_title">
+                    <h2> Aqua Orders <small>Monthly</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+         <div google-chart chart="chartObject" style="
+width:100%;"></div>
+                  </div>
+                </div>
+</div>
+<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+          <div class="x_panel">
+                  <div class="x_title">
+                    <h2> Aqua Orders <small>Monthly</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+         <div google-chart chart="chartObject" style="
+width:100%;"></div>
+                  </div>
+                </div>
 </div>
 <!--   <div class="col-lg-6 col-md-6 col-xs-12">
   <div class="containr" ng-style="setContainer()">
