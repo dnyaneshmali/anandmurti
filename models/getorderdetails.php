58a83694489b64a3a1c625c462305106a2a41434
Santosh <santosh.bhosale123@gmail.com>
2017-04-04 13:10:38 +0530

    getaquaorderdetails by desc order

--- models/getorderdetails.php
+++ models/getorderdetails.php
@@ -4,7 +4,7 @@ include($root."/config/config.php");
 $data=array();
 
 //$query="SELECT * FROM  tbl_aqua_orders order by order_delivery_date,order_delivery_time ";
-$query="select tbl_aqua_orders.*,tbl_aqua_customers.acustomer_name,tbl_aqua_customers.acustomer_id from tbl_aqua_orders INNER JOIN tbl_aqua_customers On tbl_aqua_orders.acustomer_id = tbl_aqua_customers.acustomer_id where order_activestatus = 1";
+$query="select tbl_aqua_orders.*,tbl_aqua_customers.acustomer_name,tbl_aqua_customers.acustomer_id from tbl_aqua_orders INNER JOIN tbl_aqua_customers On tbl_aqua_orders.acustomer_id = tbl_aqua_customers.acustomer_id where order_activestatus = 1 order by order_delivery_date DESC";
 
 $result = mysqli_query($connection,$query);
 
